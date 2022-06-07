86ed3a22c31c0dc94e20179b8a3c5b305a86f04b
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-07 11:30:58 +0200

    error handle

--- src/main/java/com/acme/apirest/controllers/ArticuloRestController.java
+++ src/main/java/com/acme/apirest/controllers/ArticuloRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -35,8 +36,23 @@ public class ArticuloRestController {
     }
 
     @PostMapping("/articulos")
-    public Articulo create(@RequestBody Articulo articulo) {
-        return articuloService.save(articulo);
+    public ResponseEntity<?> create(@RequestBody Articulo articulo) {
+        Articulo newArticulo = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            newArticulo = articuloService.save(articulo);
+        } catch (DataAccessException e) {
+            if (e.getMostSpecificCause().getMessage().contains("Duplicate entry")) {
+                response.put("message","Código SKU duplicado");
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+            } else {
+                response.put("message","El servidor no se encuentra disponible");
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        response.put("message","El artículo fue creado correctamente");
+        response.put("article", newArticulo);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
 }
