11da818e8aa824ddb0320826c46d472b31be5a69
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-13 14:14:54 +0200

    fixed

--- src/main/java/com/acme/apirest/models/services/ProveedorService.java
+++ src/main/java/com/acme/apirest/models/services/ProveedorService.java
@@ -3,11 +3,13 @@ package com.acme.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.acme.apirest.models.dao.IProveedorDAO;
 import com.acme.apirest.models.entity.Proveedor;
 
+@Service
 public class ProveedorService implements IProveedorService {
     // Aquí declaramos los métodos exigidos por la interfaz
     // y los enlazamos con los métodos que proporciona el DAO
